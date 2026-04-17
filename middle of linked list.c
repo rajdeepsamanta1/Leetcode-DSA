e1b39465baf8452539b6dd1388a2df652c03c130
rajdeepsamanta01 <111226964+rajdeepsamanta1@users.noreply.github.com>
2026-04-17 19:36:47 +0530

    Add files via upload

---
+++ middle of linked list.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+
+    if(head->next == NULL)
+    return head;
+
+    struct ListNode* fast_ptr = head;
+    struct ListNode* slow_ptr = head;
+
+    while(fast_ptr!=NULL && fast_ptr->next!=NULL)
+    {
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+    }
+
+    return slow_ptr;
+    
+}
